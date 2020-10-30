b1e0691e13ccc21a974fd1b3a5b755922370627f
Mikhail Ilyushin <mishamine26@mail.ru>
2020-10-30 13:50:24 +0300

    Добавлено два теста к 6

--- Labe6/PasswordWorkerTests.cs
+++ Labe6/PasswordWorkerTests.cs
@@ -13,5 +13,27 @@ namespace Labe6
             string pass = passwordWorker.GeneratePassword();
             Assert.IsTrue(pass.Length >= 7 && pass.Length <= 10);
         }
+
+        [TestMethod]
+        public void PasswordEncryptTest()
+        {
+            var passwordWorker = RandomPasswordTripleDES.Get();
+            string passRaw = "2C31pBkg6";
+            string logRaw = "JopaDrish";
+            string expected = "SRLgVbLwlwOxlnOv45BrnA==";
+            string actual = passwordWorker.Encrypt(passRaw, logRaw);
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void PasswordDecryptTest()
+        {
+            var passwordWorker = RandomPasswordTripleDES.Get();
+            string passRaw = "SRLgVbLwlwOxlnOv45BrnA==";
+            string logRaw = "JopaDrish";
+            string expected = "2C31pBkg6";
+            string actual = passwordWorker.Decrypt(passRaw, logRaw);
+            Assert.AreEqual(actual, expected);
+        }
     }
 }
