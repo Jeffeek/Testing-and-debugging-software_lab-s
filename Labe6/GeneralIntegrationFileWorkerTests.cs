27d5afba6513223fb2a9b56c38569b0139af4d16
Mikhail Ilyushin <mishamine26@mail.ru>
2020-11-05 13:58:18 +0300

    лаба 7

---
+++ Labe6/GeneralIntegrationFileWorkerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Linq;
+using CIP_lab_1.Models;
+using CIP_lab_1.Workers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Labe6
+{
+    [TestClass]
+    public class GeneralIntegrationFileWorkerTests
+    {
+        [TestMethod]
+        public void FileReaderTest()
+        {
+            var fileWorker = new FileWorker<Profile>($"{Directory.GetCurrentDirectory()}//profiles.json");
+            var profiles = fileWorker.Reader.GetProfiles();
+            Assert.IsTrue(profiles.Count > 0);
+        }
+
+        [TestMethod]
+        public void FileWriterTest()
+        {
+            var fileWorker = new FileWorker<Profile>($"{Directory.GetCurrentDirectory()}//profiles.json");
+            int count = fileWorker.Reader.GetProfiles().Count;
+            var profile = new Profile()
+            {
+                FullName = "Alesha",
+                Login = "Popovich",
+                Password = "thisIsNotValidPass",
+                SecretWord = "NotSecret"
+            };
+            fileWorker.Writer.AddNewProfile(profile);
+            var profiles = fileWorker.Reader.GetProfiles();
+            int newCount = profiles.Count;
+            Assert.IsTrue(newCount == count + 1);
+            profiles.Remove(profile);
+            fileWorker.Writer.RewriteAll(profiles);
+        }
+    }
+}
